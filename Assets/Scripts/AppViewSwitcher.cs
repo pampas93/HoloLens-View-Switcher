71381637b84842a5606e78bd8600a36defd21c2d
abhijit.93@hotmail.com <abhijit@sketchup.com>
2018-07-25 13:51:25 -0600

    Project upload

---
+++ Assets/Scripts/AppViewSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class AppViewSwitcher : MonoBehaviour
+{
+
+    public delegate void NavigateToFlatDelegate();
+    public static NavigateToFlatDelegate NavigateToFlat;
+
+    void Start()
+    {
+        StartCoroutine(NewView());
+    }
+
+    IEnumerator NewView()
+    {
+        yield return new WaitForSeconds(5);
+        if (NavigateToFlat != null)
+        {
+#if WINDOWS_UWP
+            UnityEngine.WSA.Application.InvokeOnUIThread(() =>
+            {
+                NavigateToFlat();
+            }, false);
+#endif
+        }
+    }
+}
